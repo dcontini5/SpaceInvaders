643fffe0a9035d9be46c726d3252f1d52d00e0ae
528251 <528251@FENN-177-24>
2020-02-17 15:03:55 +0000

    Added Bonus Ship Class

---
+++ CrackerChase/BonusShip.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrackerChase
+{
+    class BonusShip
+    {
+        private float speed;
+        private float position;
+        private float radius;
+
+        private static bool isRight;
+
+        // Initialize the Ship //
+        private void Initialize()
+        {
+            isRight = true;
+            // Load Content //
+
+        }
+
+        // Load the Ship Sprite //
+        private void LoadContent()
+        {
+
+        }
+
+        private void Move()
+        {
+            if (isRight)
+            {
+                // Move the Ship in the right direction //
+
+                isRight = false;
+            }
+            else
+            {
+
+            }
+
+        }
+
+
+    }
+}
