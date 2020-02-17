238b81d7d1677f2057223e51e4749fff53585e9c
dcontini5 <devcontini@gmail.com>
2020-02-17 15:33:31 +0000

    a

---
+++ CrackerChase/Projectile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CrackerChase
+{
+    class Projectile : Sprite
+    {
+
+
+        private float _ySpeed;
+
+        public Projectile(int inScreenWidth, int inScreenHeight, Texture2D inSpriteTexture, int inDrawWidth, Vector2 pos, float ySpeed) :
+            base(inScreenWidth, inScreenHeight, inSpriteTexture, inDrawWidth, pos.X, pos.Y)
+        {
+
+            _ySpeed = ySpeed;
+            base.Reset();
+
+        }
+
+        public override void Update(float deltaTime)
+        {
+            
+            yPosition = yPosition + (_ySpeed * deltaTime);
+            
+            base.Update(deltaTime);
+        }
+
+        public bool isOffScreen(){
+
+            bool offScreen = false;
+
+            if (yPosition < 0 || yPosition + rectangle.Height > screenHeight)
+            {
+                offScreen = true;
+            }
+
+
+            return offScreen;
+        }
+    }
+}
